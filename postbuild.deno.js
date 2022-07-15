035cbdb52832bc45928564aacf2f7aced1d17ed0
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-07-16 00:50:39 +0430

    fix

--- postbuild.deno.js
+++ postbuild.deno.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 
 let content = fs.readFileSync('./output/handler.js', 'utf-8')
 
-content = content.replace(/'uuid'/g, '\'https://skypack.dev/uuid\'')  
-content = content.replace(/'nanoid'/g, '\'https://skypack.dev/nanoid\'')  
+content = content.replace(/'uuid'/g, '\'https://jspm.io/uuid\'')  
+content = content.replace(/'nanoid'/g, '\'https://jspm.io/nanoid\'')  
 
 fs.writeFileSync('./output/handler.js', content)
